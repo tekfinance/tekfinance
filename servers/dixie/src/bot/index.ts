1bdff01f6b6cfbcd597b2aef4496776697e386be
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-10-02 20:40:53 +0100

    ...

--- servers/dixie/src/bot/index.ts
+++ servers/dixie/src/bot/index.ts
@@ -68,10 +68,10 @@ export class Dixie extends Service {
           command: Dixie.fundCommand,
           description: "Get wallet address and QRCode.",
         },
-        {
-          command: Dixie.referCommand,
-          description: "Refer a friend to earn points.",
-        },
+        // {
+        //   command: Dixie.referCommand,
+        //   description: "Refer a friend to earn points.",
+        // },
         // {
         //   command: Dixie.couponCommand,
         //   description: "Create and fund a new coupon",
@@ -103,14 +103,14 @@ export class Dixie extends Service {
     bot.action(Dixie.menuCommand, onMenu);
     bot.action(Dixie.fundCommand, fundCommand);
     bot.action(/chain/, chainCommand);
-    bot.action(Dixie.referCommand, referCommand);
+    // bot.action(Dixie.referCommand, referCommand);
     bot.action(Dixie.settingsCommand, settingsCommand);
     // bot.action(/^confirmRedeem/, confirmRedeemCommand);
 
     bot.command(Dixie.tipCommand, onTip);
     bot.command(Dixie.menuCommand, menuCommand);
     bot.command(Dixie.fundCommand, fundCommand);
-    bot.command(Dixie.referCommand, referCommand);
+    // bot.command(Dixie.referCommand, referCommand);
     // bot.command(Dixie.redeemCommand, redeemCommand);
     // bot.command(Dixie.couponCommand, couponCommand);
     bot.command(Dixie.settingsCommand, settingsCommand);
